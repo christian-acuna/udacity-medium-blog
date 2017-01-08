cfb5ad25254d76805bb653a97f3226c791a3353a
Christian Acuña <cacuna0828@gmail.com>
2017-01-08 12:11:34 -0800

    fix blog to show content

--- medium.py
+++ medium.py
@@ -9,6 +9,15 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                 autoescape = True)
 
+# def blog_key(name = 'default'):
+#     return db.Key.from_path('blogs', name)
+
+class Post(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
@@ -20,10 +29,14 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-class Post(db.Model):
-    subject = db.StringProperty(required = True)
-    content = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
+
+class MainPage(Handler):
+    def render_front(self):
+        posts = Post.all().order('-created')
+        self.render("front.html", posts = posts)
+
+    def get(self):
+        self.render_front()
 
 class NewPost(Handler):
     def render_form(self, subject="", content="", error=""):
@@ -45,25 +58,22 @@ class NewPost(Handler):
             self.redirect('/blog/' + str(id) )
         else:
             error = 'A post needs both a subject line and content'
-            self.render_form(subject, content, error)
+            self.render_form(subject=subject, content=content, error=error)
 
-
-class MainPage(Handler):
-    def render_front(self):
-        posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC")
-
-        self.render("front.html", posts = posts)
-
-    def get(self):
-        self.render_front()
 
 class PostHandler(Handler):
     def render_post(self, post_id):
-        post = Post.get_by_id(int(post_id))
+        key = db.Key.from_path('Post', int(post_id))
+        post = db.get(key)
 
-        self.render("post.html", post = [post])
+        if not post:
+            self.error(404)
+            return
+
+        # post = Post.get_by_id(int(post_id))
+        self.render("post.html", post = post)
 
     def get(self, post_id):
         self.render_post(post_id)
 
-app = webapp2.WSGIApplication([('/blog', MainPage), ('/blog/newpost', NewPost), (r'/blog/(\d+)', PostHandler)], debug=True)
+app = webapp2.WSGIApplication([('/blog/?', MainPage), ('/blog/newpost', NewPost), (r'/blog/(\d+)', PostHandler)], debug=True)
