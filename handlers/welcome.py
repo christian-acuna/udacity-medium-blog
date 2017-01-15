994dc56150cb087ba979849d933fcd3eb6996407
Christian Acuña <cacuna0828@gmail.com>
2017-01-15 14:01:11 -0800

    add error and suceess messages for deleting a comment

--- handlers/welcome.py
+++ handlers/welcome.py
@@ -5,6 +5,9 @@ from models.comment import Comment
 
 class WelcomeHandler(Handler):
     def get(self):
+        message = self.request.get('msg')
+        if message == '1':
+            message = "Your post has been successfully deleted"
         # self.user is set in initialize function in Handler
         if self.user:
             q_posts = db.Query(Post)
@@ -13,6 +16,7 @@ class WelcomeHandler(Handler):
             comments = q_comments.filter('author_id =', self.user.key().id())
             self.render('welcome.html', username = self.user.username,
                                         posts = posts,
-                                        comments = comments)
+                                        comments = comments,
+                                        message = message)
         else:
             self.redirect('/signup')
